24ee7465285d199fe4efc115ca4579568e305949
Laura Wentzell Ahmad <laura.s.ahmad@gmail.com>
2017-09-28 13:47:26 -0500

    adding changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-// var Questions = {
+
 // quest1: {
 // Who was the legendary Benedictine monk who invented champagne?: 
 // ["Dom Perignon", "Jaques Champaine", "Perrier Blanchard", "Monti Christo"],
@@ -42,13 +42,27 @@
 // quest11: {
 // Who invented the rabies vaccination?: 
 // ["Louis Pasteur", "Charles DuBois", "Linus Pauling", "Annette Dauphani"],
-// }
+// 
+var count = 60;
+var time = setInterval(timer, 1000);
+
+function timer(){
+	count--;
+	$("#timer").html(count);
+	if(count == 0){
+		 clearInterval(time);
+		 $("#timer").append("<h1>Time's Up!</h1>");
+		 check();
+		}
+	}
 
 function check(){
 	var question1 = document.trivia.question1.value;
 	var question2 = document.trivia.question2.value;
 	var question3 = document.trivia.question3.value;
 	var question4 = document.trivia.question4.value;
+	var question5 = document.trivia.question5.value;
+	var question6 = document.trivia.question6.value;
 	var correct = 0;
 	var incorrect= 0;
 
@@ -72,7 +86,18 @@ function check(){
 	}else {
 		incorrect++;
 	}
-
+	if (question5 == "A Mackintosh"){
+		correct++;
+	}else {
+		incorrect++;
+	}
+	if (question6 == "A sword"){
+		correct++;
+	}else {
+		incorrect++;
+	}
+	document.getElementById("timer").style.visibility="hidden";
+	document.getElementById("trivia").style.visibility="hidden";
 	document.getElementById("afterCheck").style.visibility="visible";
 	document.getElementById("numberCorrect").innerHTML="You got "+ correct + " correct!";
 	document.getElementById("numberIncorrect").innerHTML="You got "+ incorrect + " incorrect!";
